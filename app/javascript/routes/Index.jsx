b1b14038e4ddfbb962ac1cd49645c97c28d28137
David <dhahn123@gmail.com>
2019-11-03 17:41:09 -0800

    update routes

--- app/javascript/routes/Index.jsx
+++ app/javascript/routes/Index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "../components/Home";
+import NewRecipe from "../components/NewRecipe";
 import Recipe from "../components/Recipe";
 import Recipes from "../components/Recipes";
 
@@ -10,6 +11,7 @@ export default (
       <Route path="/" exact component={Home} />
       <Route path="/recipes" exact component={Recipes} />
       <Route path="/recipe/:id" exact component={Recipe} />
+      <Route path="/recipe" exact component={NewRecipe} />
     </Switch>
   </Router>
 );
